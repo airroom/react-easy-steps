29de19025cae1a152428b87b3316224bed284ea0
Romário Lima <romario.lima@easynvest.com.br>
2017-05-08 17:28:46 -0300

    Um vislumbre a zueira

--- __stories__/index.js
+++ __stories__/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { storiesOf } from '@kadira/storybook'
 import { EasySteps } from '../src'
 
-storiesOf('<EasySteps />', module)
-.add('default', () =>
-  <EasySteps />
+const FirstSlide = (props, context) => (
+  <div>
+    <p>Vai simbora</p>
+    <button onClick={context.goToNext}>
+      Click Me
+    </button>
+  </div>
 )
+
+FirstSlide.contextTypes = {
+  goToNext: PropTypes.func
+}
+
+storiesOf('<EasySteps />', module)
+.add('default', () => (
+  <EasySteps>
+    <FirstSlide />
+    <div>
+      Olar
+    </div>
+    <div>
+      Opãn
+    </div>
+    <div>
+      Como tar
+    </div>
+  </EasySteps>
+))
--- src/EasySteps.js
+++ src/EasySteps.js
@@ -1,6 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-const EasySteps = () =>
-  <div>Sou Alérgico a Crustáceos</div>
+export default class EasySteps extends Component {
+  static childContextTypes = {
+    goTo: PropTypes.func,
+    goToNext: PropTypes.func,
+    goToPrev: PropTypes.func
+  }
 
-export default EasySteps
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+
+    this.goTo = this.goTo.bind(this)
+    this.goToNext = this.goToNext.bind(this)
+    this.goToPrev = this.goToPrev.bind(this)
+  }
+
+  getChildContext() {
+    return {
+      goTo: this.goTo,
+      goToNext: this.goToNext,
+      goToPrev: this.goToPrev
+    }
+  }
+
+  goTo(slideNumber) {
+    this.setState({
+      currentSlideIndex: slideNumber
+    })
+  }
+
+  goToNext() {
+    this.goTo(this.state.currentSlideIndex + 1)
+  }
+
+  goToPrev() {
+    this.goTo(this.state.currentSlideIndex - 1)
+  }
+
+  render() {
+    const { children } = this.props
+
+    return (
+      <div>
+        {
+          React.cloneElement(
+            children[this.state.currentSlideIndex]
+          )
+        }
+      </div>
+    )
+  }
+}
